c6579cd7783c82439f05843215bf9af11cd758bb
LuisPuchol2DAW <lpucholdelgado@cifpfbmoll.eu>
2024-12-18 20:50:26 +0100

    aparecen los numeros al generarse el tablero

--- app/src/main/java/com/example/esjoguet/Activity_2048Game.java
+++ app/src/main/java/com/example/esjoguet/Activity_2048Game.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.widget.TextView;
 
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,23 +16,147 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.esjoguet.databinding.Activity2048GameBinding;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Activity_2048Game extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
     private static final String TAG = "MainActivity";
     private GestureDetector gestureDetector;
     private float startX, startY, endX, endY;
 
+
+    private final int GRID_SIZE = 4; // Tamaño del tablero (4x4)
+    private Integer[][] board; // Representa los valores del tablero
+    private TextView[][] tiles; // Almacena las referencias a los ImageViews
+    private Random random = new Random(); // Para elegir casillas y valores aleatorios
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate: Activity has been created");
-
         super.onCreate(savedInstanceState);
-
         startStuff(savedInstanceState);
-
         gestureDetector = new GestureDetector(this,this);
 
+        board = new Integer[GRID_SIZE][GRID_SIZE]; //Matriz vacia
+        tiles = new TextView[GRID_SIZE][GRID_SIZE]; //almacena los valores de las casillas
+
+        tiles[0][0] = findViewById(R.id.tv1x1);
+        tiles[0][1] = findViewById(R.id.tv1x2);
+        tiles[0][2] = findViewById(R.id.tv1x3);
+        tiles[0][3] = findViewById(R.id.tv1x4);
+
+        tiles[1][0] = findViewById(R.id.tv2x1);
+        tiles[1][1] = findViewById(R.id.tv2x2);
+        tiles[1][2] = findViewById(R.id.tv2x3);
+        tiles[1][3] = findViewById(R.id.tv2x4);
+
+        tiles[2][0] = findViewById(R.id.tv3x1);
+        tiles[2][1] = findViewById(R.id.tv3x2);
+        tiles[2][2] = findViewById(R.id.tv3x3);
+        tiles[2][3] = findViewById(R.id.tv3x4);
+
+        tiles[3][0] = findViewById(R.id.tv4x1);
+        tiles[3][1] = findViewById(R.id.tv4x2);
+        tiles[3][2] = findViewById(R.id.tv4x3);
+        tiles[3][3] = findViewById(R.id.tv4x4);
+
+        spawnRandomTile();
+        spawnRandomTile();
     }
+
+    private Boolean isEmptyTile(Integer tileValue) {
+        return tileValue == null;
+    }
+
+    private void spawnRandomTile() {
+        ArrayList<Integer[]> emptyTiles = new ArrayList<>();
+
+        // Busca las casillas vacías en el tablero
+        for (Integer i = 0; i < GRID_SIZE; i++) {
+            for (Integer j = 0; j < GRID_SIZE; j++) {
+                if (isEmptyTile(board[i][j])) {
+                    emptyTiles.add(new Integer[]{i, j});
+                }
+            }
+        }
+
+        if (emptyTiles.isEmpty()) {
+            return; // No hay casillas vacías, el juego ha terminado
+        }
+
+        Integer[] position = emptyTiles.get(random.nextInt(emptyTiles.size()));
+        Integer row = position[0];
+        Integer col = position[1];
+
+        Integer value = random.nextFloat() < 0.9 ? 2 : 4; // 90% de probabilidad de 2, 10% de probabilidad de 4
+
+        board[row][col] = value;
+
+        updateTileUI(row, col, value);
+
+        // Encuentra una posición vacía aleatoria y asigna un 2 o 4
+    }
+
+    private void updateTileUI(Integer row, Integer col, Integer value) {
+        TextView tile = tiles[row][col];
+        if (value > 0) {
+            tile.setText(String.valueOf(value)); // Muestra el número
+        } else {
+            tile.setText(""); // Deja vacío si el valor es 0
+        }
+        switch (value) {
+            case 2:
+                tile.setBackgroundResource(R.color.tile_2);
+                break;
+            case 4:
+                tile.setBackgroundResource(R.color.tile_4);
+                break;
+            case 8:
+                tile.setBackgroundResource(R.color.tile_8);
+                break;
+            case 16:
+                tile.setBackgroundResource(R.color.tile_16);
+                break;
+            default:
+                tile.setBackgroundResource(R.color.tile_default);
+                break;
+        }
+    }
+
+    private void moveTiles(int direction) {
+        // Realiza el movimiento de las piezas en la dirección especificada
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public void startStuff(Bundle savedInstanceState) {
         EdgeToEdge.enable(this);
